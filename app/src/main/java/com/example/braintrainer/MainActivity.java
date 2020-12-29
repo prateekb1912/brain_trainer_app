be70a719e676e2c4c1bcd16e8452c1c7e7c887a2
prateek-ml <prateekb1912@gmail.com>
2020-12-29 19:08:03 +0530

    Countdown timer and restart game button

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,28 +14,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button goButton;
-    TextView sumTextView;
-    ArrayList<Integer> possibleAnswers = new ArrayList<Integer>();
+    Button goButton, restartButton;
+    TextView sumTextView, responseTextView, scoreTextView, timerTextView;
     ArrayList<Button> optionButtons = new ArrayList<Button>();
-    int correctOption;
-
-    public void chooseAnswer(View view) {
-        String correct = Integer.toString(correctOption);
-        String tag = view.getTag().toString();
-
-        if(correct.equals(tag)) {
-            Log.i("Correct", "You got it!");
-        }
-        else {
-            Log.i("Oh no", ":(");
-        }
-    }
-
-    public void startGame(View view) {
-        goButton.setVisibility(View.INVISIBLE);
-    }
-
+    int correctOption, score = 0, questions = 0;
     private static final int[] BUTTON_IDS = {
             R.id.button1,
             R.id.button2,
@@ -42,20 +25,35 @@ public class MainActivity extends AppCompatActivity {
             R.id.button4,
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void playAgain(View view) {
+        score = 0;
+        questions = 0;
+        timerTextView.setText("30s");
+        new CountDownTimer(30100, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText(Long.toString(millisUntilFinished/1000) + "s");
+            }
 
-        goButton = findViewById(R.id.goButton);
-        sumTextView = findViewById(R.id.sumTextView);
+            @Override
+            public void onFinish() {
+                responseTextView.setText("DONE!");
+                restartButton.setVisibility(View.VISIBLE);
+            }
+        }.start();
+    }
 
+    public void newQuestion() {
+
+        ArrayList<Integer> possibleAnswers = new ArrayList<Integer>();
         Random rand = new Random();
 
         int a = rand.nextInt(21);
         int b = rand.nextInt(21);
         int sum = a+b;
         correctOption = rand.nextInt(4);
+
+        newQuestion();
 
         sumTextView.setText(Integer.toString(a)+" + "+Integer.toString(b));
 
@@ -75,6 +73,42 @@ public class MainActivity extends AppCompatActivity {
 
             optionButtons.get(i).setText(Integer.toString(possibleAnswers.get(i)));
         }
+    }
 
+    public void chooseAnswer(View view) {
+        String correct = Integer.toString(correctOption);
+        String tag = view.getTag().toString();
+
+        if(correct.equals(tag)) {
+            responseTextView.setText("Correct!");
+            score++;
+            questions++;
+        }
+        else {
+            responseTextView.setText("Wrong :(");
+            questions++;
+        }
+        scoreTextView.setText(Integer.toString(score)+"/"+Integer.toString(questions));
+        newQuestion();
+    }
+
+    public void startGame(View view) {
+        goButton.setVisibility(View.INVISIBLE);
+        newQuestion();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        goButton = findViewById(R.id.goButton);
+        sumTextView = findViewById(R.id.sumTextView);
+        responseTextView = findViewById(R.id.responseTextView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        timerTextView = findViewById(R.id.timerTextView);
+        restartButton = findViewById(R.id.restartButton);
+
+        playAgain(timerTextView);
     }
 }

254bcd553ec99c9034a7d502ac48c5e3d1104b9d
prateek-ml <prateekb1912@gmail.com>
2020-12-29 17:46:25 +0530

    App created

---
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button goButton;
+
+    public void start(View view) {
+        goButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
